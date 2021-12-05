d2c039c465b43ea51442d68b0d66fe60d305376c
Dau Cheng <frank.d.cheng@gmail.com>
2021-12-04 19:24:31 -0800

    idyll file

---
+++ components/tableau-embed.js
@@ -0,0 +1,18 @@
+const React = require('react');
+
+class TableauEmbed extends React.Component {
+  render() {
+    const { hasError, idyll, updateProps, ...props } = this.props;
+    return (
+      <iframe 
+        src={this.props.link + ':showVizHome=no&:embed=true'}
+        scrolling={"no"}
+        style={{border:"none"}}
+        width={this.props.width} 
+        height={this.props.height}
+      />
+    );
+  }
+}
+
+module.exports = TableauEmbed;
